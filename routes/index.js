df3f7cee0712af58cee3d50ec86a191b844c0c4d
Khsrla9806 <kimhunsope@naver.com>
2024-12-26 16:42:01 +0900

    feat: Database 연동 (MySQL) 및 설정 정보 추가

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+let db = require('../lib/db.js');
 
 // ROOT 경로에 대한 라우터 설정
-router.get('/', (req, res) => {
-    res.json({
-        message: 'Welcome Rabbit Server!'
-    });
+router.get('/', async (req, res, next) => {
+    try {
+        const result = await db.query("SELECT 'HOME' FROM DUAL");
+        res.json({
+            message: 'Welcome Rabbit Server!',
+            results: result
+        });
+    } catch (error) {
+        console.error(error);
+        next(error); // 에러를 다음 미들웨어로 전달
+    }
 });
 
 // 외부에서 사용할 수 있도록 export
